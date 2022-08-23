ea258c169b27fa6d526f14a2cfe9c1791eab69f7
Mezzky <ryansahadha@gmail.com>
2022-08-23 14:54:42 +0800

    Add: Navbar Toggle

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// SIDEBAR
+const navShow =  document.querySelector("header");
+const navToggle =  document.querySelector("header nav h1");
+navToggle.addEventListener('click', function(){
+navShow.classList.toggle("active")
+});
